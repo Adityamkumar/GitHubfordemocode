82d973ad9f9ea57d4c63f5cbe7e6314e78206463
Aditya kumar <134033409+Adityamkumar@users.noreply.github.com>
2023-06-03 08:15:03 +0530

    Update array fun part 3.py

--- array fun part 3.py
+++ array fun part 3.py
@@ -4,5 +4,5 @@ matrix=np.random.randint(1,21,10)
 np.random.shuffle(matrix)#it will shuffle the matrix values
 print(matrix)
 a=np.array([10,20,30,10,30,20,50])
-print(np.unique(a.size))#UNIQUE function will remove the dupliicates from array,if we will give (.size) it will teli you the size of array#
+print(np.unique(a.size))#UNIQUE function will remove the dupliicates from array,if we will give (.size) it will tell you the size of array#
 print(np.unique(a,return_index=True,return_counts=True))#this function returns the index value and frequency of numbers of unique elements#
