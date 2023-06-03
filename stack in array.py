d197c8e53e4c25b16240bf06f3457056b96ad319
Aditya kumar <134033409+Adityamkumar@users.noreply.github.com>
2023-06-03 08:09:10 +0530

    Update stack in array.py

--- stack in array.py
+++ stack in array.py
@@ -2,5 +2,5 @@ import numpy as np
 np.random.seed(122)
 a=np.random.randint(1,21,9).reshape(3,3)
 b=np.random.randint(31,51,9).reshape(3,3)
-print(np.hstack((a,b)))#horizontal stacking to combine two matrix horizontally
-print(np.vstack((a,b)))#vertical stacking to combine two vertically
+print(np.hstack((a,b)))#horizontal stacking  combine two matrix horizontally
+print(np.vstack((a,b)))#vertical stacking  combine two matrix vertically
